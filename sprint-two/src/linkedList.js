68b5b45c6c5bc078fc85f10d7e003d70e046e21a
Estelle Johnson <estellemarjohnson@gmail.com>
2022-11-05 12:32:32 -0700

    Complete Linked List

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,48 @@
+
 var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode1 = new Node(value);
+    if (!list.head) {
+      list.head = newNode1;
+      list.tail = list.head;
+    }
+
+    list.tail.next = newNode1;
+    list.tail = list.tail.next;
   };
+
 
   list.removeHead = function() {
+
+    var headValue = list.head;
+
+    delete list.head;
+    list.head = headValue.next;
+
+    return headValue.value;
   };
 
+
   list.contains = function(target) {
+
+    var result = false;
+
+    var hasTarget = function(obj) {
+
+      if (obj.value === target) {
+        result = true;
+      }
+
+      if (obj.next !== null) {
+        hasTarget(obj.next);
+      }
+    };
+    hasTarget(list.head);
+    return result;
   };
 
   return list;
@@ -22,6 +55,7 @@ var Node = function(value) {
   node.next = null;
 
   return node;
+
 };
 
 /*

feae28e4b7c57e54dbfda4d363183fa92fa4b59c
Mallow <linlam0923@gmail.com>
2022-11-12 22:22:44 -0500

    Complete Hash Table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -4,52 +4,45 @@ var HashTable = function() {
   this.limit = 8;
   this.storage = LimitedArray(this.limit);
 };
-
-HashTable.prototype.insert = function(k, v) { //k = val1 , value = val1 ;; key = val2, value = val2
+HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this.limit); //0
-  // var tuple = [k, v];
+  var tuple = [k, v];
+  var bucket = this.storage.get(index);
 
-  // if (this.storage[index]) {
-  //   this.storage[index].push(tuple);
-  // } else {
-  //   this.storage.set(index, tuple);
-  // }
-  // var bucket = this.storage.get(index); //
-
-  // console.log('current storage', this.storage)
-
-  // if (!bucket) {
-  //   this.storage.set(index, tuple);
-  //   console.log (this.storage)
-  // }
-  // bucket.push(tuple);
-
-  if (this.storage[index] === undefined) {
-    this.storage[index] = {};
+  if (!bucket) {
+    bucket = [tuple];
+    this.storage.set(index, bucket);
   }
-
-  this.storage[index][k] = v; //{0 : {val1: val1, val2: val2}}
-
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      bucket[i][1] = v;
+    } else {
+      bucket.push(tuple);
+    }
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
-  var index = getIndexBelowMaxForKey(k, this.limit);
+  var index = getIndexBelowMaxForKey(k, this.limit); //0
+  var bucket = this.storage.get(index);
 
-  return this.storage[index][k];
-
-  // return this.storage[index].get(k);
-  // var bucket = this.storage.get(index);
-  // for (var i = 0; i < bucket.length; i++) {
-  //   if (bucket[i][0] === k) {
-  //     return bucket[i][1];
-  //   }
-  // }
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      return bucket[i][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this.limit);
+  var bucket = this.storage.get(index);
 
-  delete this.storage[index][k];
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      bucket.splice(i, 1);
+    }
+  }
+  // delete this.storage[index][k];
 };
 
 
